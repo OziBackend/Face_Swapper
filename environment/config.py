4230c5f985c5d9cef79f3d5b2ed099e507c4c747
oziBackend <ozibackend@gmail.com>
2025-10-17 11:00:12 +0500

    Config file setup completed

--- environment/config.py
+++ environment/config.py
@@ -1,13 +1,20 @@
-PORT = 9002
-IP = "172.16.0.94"
+CURRENT_SYSTEM_IP = "172.16.0.94"
+CURRENT_SYSTEM_USER_DIRECTORY = "C:/Users/muhammadannasasif/"
 
+#Port and IP for the server
+PORT = 9002
+IP = CURRENT_SYSTEM_IP
+
+#Model Paths
 MODEL_PATH = "models/inswapper_128.onnx"
 DETECTION_MODEL_NAME = "buffalo_l"
-DETECTION_MODEL_ROOT = "C:/Users/muhammadannasasif/.insightface"
+DETECTION_MODEL_ROOT = f"{CURRENT_SYSTEM_USER_DIRECTORY}.insightface"
 DETECTION_MODEL_CTX_ID = 0
 
+#Templates and Static Files Paths
 TEMPLATES_DIR = "templates"
 STATIC_DIR = "static"
 STATIC_PATH = "static"
 
+#Image URL Prefix
 IMAGE_URL_PREFIX = f"http://{IP}:{PORT}/read_image?file_name="
